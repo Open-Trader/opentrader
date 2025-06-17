648173af9ca2e6ad0db7fa2fedffc72bc9af51d0
bludnic <victorcpla@gmail.com>
2025-06-17 14:35:42 +0100

    chore(ccxt): add a type guard to check what exchanges were removed/added

--- packages/exchanges/src/client/constants.ts
+++ packages/exchanges/src/client/constants.ts
@@ -2,6 +2,13 @@ import { pro as ccxt } from "ccxt";
 import { ExchangeCode } from "@opentrader/types";
 
 /**
+ * Just a guard. Uncomment this to see what exchanges were added or removed from CCXT.
+ */
+// type EnsureEqual<T, U extends T = T> = T extends U ? true : never;
+// type EnsureNoDeletedExchanges = EnsureEqual<ExchangeCode, Uppercase<keyof typeof exchanges>>;
+// type EnsureNotNewExchangesAdded = EnsureEqual<Uppercase<keyof typeof exchanges>, ExchangeCode>;
+
+/**
  * Map exchange code to CCXT instance class name
  */
 export const exchangeCodeMapCCXT: Record<ExchangeCode, keyof typeof ccxt> = {
